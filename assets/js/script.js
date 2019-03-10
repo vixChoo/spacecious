52b0d38b5d526dc3faf070a129f090f3e2417c28
Vixtrum <vixtrumchoo@yahoo.com>
2019-03-10 19:43:44 +0800

    Redo product profile to add zip code

--- assets/js/script.js
+++ assets/js/script.js
@@ -295,6 +295,31 @@ function debounce(func, wait, immediate) {
   };
 };
 
+
+$('.owl-carousel-1').owlCarousel({
+  loop: false,
+  rewind:true,
+  margin: 20,
+  nav: true,
+  autoplay: true,
+  autoplayTimeout: 5000,
+  autoplayHoverPause: true,
+  responsive: {
+    0: {
+      items: 1
+    },
+    600: {
+      items: 2
+    },
+   
+    1000: {
+      items: 3
+    }
+  }
+})
+
+
+
 $('.owl-carousel').owlCarousel({
   loop: true,
   margin: 20,
@@ -335,4 +360,3 @@ function hideForm(hideForm, hideCheck) {
   }
 
 }
-
