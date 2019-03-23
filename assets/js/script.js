9d03ef4e436020a03200de62d9b1234acb08212a
Vixtrum <vixtrumchoo@yahoo.com>
2019-03-24 01:21:29 +0800

    EDITing

--- assets/js/script.js
+++ assets/js/script.js
@@ -420,7 +420,7 @@ $('.owl-carousel-1').owlCarousel({
 
 
 
-$('.owl-carousel').owlCarousel({
+$('.owl-carousel-3').owlCarousel({
   loop: true,
   margin: 20,
   nav: true,
@@ -440,3 +440,26 @@ $('.owl-carousel').owlCarousel({
   }
 })
 
+
+
+$('.owl-carousel-2').owlCarousel({
+  loop: true,
+  rewind: true,
+  margin: 20,
+  nav: true,
+  autoplay: true,
+  autoplayTimeout: 5000,
+  autoplayHoverPause: true,
+  responsive: {
+    0: {
+      items: 2
+    },
+    600: {
+      items: 3
+    },
+
+    1000: {
+      items: 4
+    }
+  }
+})
