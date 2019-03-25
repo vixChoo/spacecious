0075b4476b07de820e63f811125c06416f5845a9
Vixtrum <vixtrumchoo@yahoo.com>
2019-03-25 19:15:43 +0800

    Navbar DONE for landing page

--- assets/js/script.js
+++ assets/js/script.js
@@ -557,3 +557,20 @@ $('.owl-carousel-3').owlCarousel({
     }
   }
 })
+
+
+    $(document).ready(function () {
+      $("#sidebar").mCustomScrollbar({
+        theme: "minimal"
+      });
+
+      $('#sidebarCollapse').on('click', function () {
+        $('#sidebar, #content').toggleClass('active');
+        $('.collapse.in').toggleClass('in');
+        $('a[aria-expanded=true]').attr('aria-expanded', 'false');
+      });
+    
+    });
+
+
+       
